c9ba3403af1a599529e8ec3193bf6e4754495200
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2024-01-23 18:05:17 -0300

    registration form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -8,7 +8,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 const claimUsernameFormSchema = z.object({
   username: z
     .string()
-    .min(3, { message: 'O usuário precisa ter pelo menos 3 digítos.' })
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
     .regex(/^([a-z\\-]+)$/i, {
       message: 'O usuário pode ter apenas letras e hifens.',
     })
