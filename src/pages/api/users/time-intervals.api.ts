486fdc38920d58ee6f412d9bd4535070790354ef
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2024-02-18 19:42:26 -0300

    persisting intervals

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -3,19 +3,19 @@ import { unstable_getServerSession } from 'next-auth'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse,
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-    if (req.method !== 'POST') {
-        return res.status(405).end()
-    }
-    const session = await unstable_getServerSession(
-        req,
-        res,
-        buildNextAuthOptions(req, res),
-    )
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+  const session = await unstable_getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
 
-    return res.json({
-        session,
-    })
+  return res.json({
+    session,
+  })
 }
