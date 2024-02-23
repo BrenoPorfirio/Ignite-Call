693e39a32230d7a8384856659a610bc361923eee
Breno Porfirio <brenoporfirio03@gmail.com>
2024-02-22 21:48:08 -0300

    confirmation page

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,41 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+import { CalendarBlank, Clock } from 'phosphor-react'
+
+export function ConfirmStep() {
+  function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          10 de outubro de 2024
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de email</Text>
+        <TextInput type="email" placeholder="exemplo@gmail.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button">Cancelar</Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
